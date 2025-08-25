cb46aefa439ac8fc53bbe7673d22352049a77943
Khalid Hersi <cipherpolfoundation@gmail.com>
2025-08-25 19:10:57 +0100

    backend configured for terraform state

---
+++ terraform/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "klds-ecs-threat-composer-tool-s3-bucket"
+    key            = "terraform/ecs-threat-composer-tool.tfstate"
+    region         = "eu-west-2"
+    encrypt        = true
+    dynamodb_table = "terraform-locks"
+  }
+}
