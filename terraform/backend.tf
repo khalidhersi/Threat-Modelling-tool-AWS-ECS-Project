b20330b2840df8fde2fd7b4dc178ac23ee99e91d
Khalid Hersi <cipherpolfoundation@gmail.com>
2025-08-26 14:30:29 +0100

    remove backend to allow tf build  s3 bucket

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "klds-ecs-threat-composer-tool-s3-bucket"
-    key            = "terraform/ecs-threat-composer-tool.tfstate"
-    region         = "eu-west-2"
-    encrypt        = true
-  }
-}
